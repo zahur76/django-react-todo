a8f9926c15bb81e7fbddbefd610eff53f35c1dbc
zahur <zahurmeerun@hotmail.com>
2021-12-01 22:30:46 +0400

    add static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -141,7 +141,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(ROOT_DIR, 'frontend/build/static')
 ]
-print("base dir path", ROOT_DIR)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
