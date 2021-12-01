c3eae66a01905f5ad0f0fcda9c977513c9ab433e
zahur <zahurmeerun@hotmail.com>
2021-12-02 00:02:32 +0400

    add gunicorn file

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn backend.wsgi:application
+release: python manage.py migrate
+web: gunicorn backend.wsgi --log-file -
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
-     re_path('.*',TemplateView.as_view(template_name='index.html')) 
+    re_path('.*',TemplateView.as_view(template_name='index.html'))
 ]
