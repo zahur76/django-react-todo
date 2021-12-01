6d8d49872abb2c205446d5d4b8a603c7b8696f30
zahur <zahurmeerun@hotmail.com>
2021-12-01 13:18:13 +0400

    add proxy to frontend

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from .serializers import TodoSerializer
+from django.core.serializers import serialize
 from .models import Todo
 
 # Create your views here.
 def home(request):
+    
+    data = serialize('json', Todo.objects.all())
+    print(data)
     todo = Todo.objects.all()
     context = {
         'todo': todo,
+        'data': data,
     }
     return render(request, 'home/index.html', context)
