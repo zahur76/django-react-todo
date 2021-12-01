b0de623924cdc34b6b228a7ef9f8d81f5d198ada
zahur <zahurmeerun@hotmail.com>
2021-12-01 13:06:22 +0400

    add django rest framework

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/index.html')
+    todo = Todo.objects.all()
+    context = {
+        'todo': todo,
+    }
+    return render(request, 'home/index.html', context)
