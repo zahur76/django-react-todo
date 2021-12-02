db23bba276df2ef81c8ef8b05bba046768b68b83
zahur <zahurmeerun@hotmail.com>
2021-12-02 15:15:53 +0400

    add onchange function for password input

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,6 +9,19 @@ function Header() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const [login, setLogin] = useState(false);
+
+    const [username, setUsername] = useState(null);
+    const [password, setPassword] = useState(null);
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value)
+        console.log(username)        
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+        console.log(password)        
+    }
     return (
         <div>
             <Row className="Header m-0 bg-dark text-white p-1">
@@ -22,8 +35,8 @@ function Header() {
                 </Modal.Header>                
                 <Modal.Body>
                     <form className="w-75 mx-auto form-add-todo">     
-                        <input className="col-12 m-1" type="text" placeholder="username" required/>
-                        <input className="col-12 m-1" type="password" placeholder="password" required/>  
+                        <input className="col-12 m-1" type="text" username={username} onChange={handleUsernameChange} placeholder="username" required/>
+                        <input className="col-12 m-1" type="password" password={password} onChange={handlePasswordChange} placeholder="password" required/>  
                         <input className="col-12 btn bg-dark text-light mt-2" type="submit" value="Submit" />                
                     </form>  
                 </Modal.Body>                
