e4026ea899b052cce587aa86af41c4f611ccea9c
zahur <zahurmeerun@hotmail.com>
2021-12-02 15:07:00 +0400

    add login modal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,34 @@
-import { React, useState, useEffect, useState} from "react";
+import { React, useState, useEffect} from "react";
 import './Header.css';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Modal from 'react-bootstrap/Modal'
 
 function Header() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const [login, setLogin] = useState(false);
     return (
-        <Row className="Header m-0 bg-dark text-white p-1">
-            <Col className="h1 my-auto" xs={0} md={2}></Col>
-            <Col className="h1 my-auto" xs={9}md={8}>Django-React Todo API</Col>
-            <Col className="my-auto" xs={3} md={2}><div className="btn text-light login-text">{login ? 'Login' : 'Logout'}</div></Col>
-        </Row>
+        <div>
+            <Row className="Header m-0 bg-dark text-white p-1">
+                <Col className="h1 my-auto" xs={0} md={2}></Col>
+                <Col className="h1 my-auto" xs={9}md={8}>Django-React Todo API</Col>
+                <Col className="my-auto" xs={3} md={2}><div onClick={handleShow} className="btn text-light login-text">{login ? 'Logout' : 'Login'}</div></Col>
+            </Row>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header className="p-2 mx-auto" closeButton>
+                    <Modal.Title>Login</Modal.Title>                    
+                </Modal.Header>                
+                <Modal.Body>
+                    <form className="w-75 mx-auto form-add-todo">     
+                        <input className="col-12 m-1" type="text" placeholder="username" required/>
+                        <input className="col-12 m-1" type="password" placeholder="password" required/>  
+                        <input className="col-12 btn bg-dark text-light mt-2" type="submit" value="Submit" />                
+                    </form>  
+                </Modal.Body>                
+            </Modal>
+        </div>
     );
 }
 export default Header;
