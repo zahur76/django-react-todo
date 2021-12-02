cff0d591e6a3216996ef18d2eabe2162d96908aa
zahur <zahurmeerun@hotmail.com>
2021-12-02 13:40:36 +0400

    remove django_heroku

--- backend/settings.py
+++ backend/settings.py
@@ -151,6 +151,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import django_heroku
-django_heroku.settings(locals())
