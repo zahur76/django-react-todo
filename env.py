ff5aa5db2d51af98ec973797992473ec04d65313
zahur <zahurmeerun@hotmail.com>
2021-12-03 07:38:31 +0400

    Untrack files in .gitignore

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "0z$f-!gua)gd+u54gkf45&m@e6hpz9m=le^prklt*%#bsqqc1j")
-os.environ.setdefault("DEVELOPMENT", "")
-os.environ.setdefault("DATABASE_URL", "postgres://wmuwbizaerfpba:2d242ec6af77ee4161edc363b6d177af0e5cf0b0c2e1f974ff17343fed098d4f@ec2-34-242-89-204.eu-west-1.compute.amazonaws.com:5432/d5r9rr6da8c1tb")
